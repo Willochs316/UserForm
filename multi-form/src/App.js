7a6f3de01c8aa84a1f86436593ad290cc9b80103
Willochs316 <willochs316@gmail.com>
2021-10-27 10:10:38 +0200

    first commit

---
+++ multi-form/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import UserForm from './Components/UserForm';
+
+class App extends Component {
+  render(){
+  return (
+    <div className="App">
+      <UserForm />
+    </div>
+  );
+}
+}
+
+export default App;
